76e82c4e442d80b01eec370d5ec00267dc3298f9
Siddhant Sehgal <sehgal.siddhant999@gmail.com>
2019-09-29 22:31:55 +0530

    Adding Source Files

---
+++ src/Components/Heading/Heading.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Heading.css";
+
+class Heading extends Component {
+  render() {
+    return (
+      <div className="Heading">
+        <h1>Drag & Drop Grid Layout in React</h1>
+      </div>
+    );
+  }
+}
+
+export default Heading;
